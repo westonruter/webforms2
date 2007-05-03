e0fca21ea92da67ee153e92eb651ba83cd014712
Weston Ruter <westonruter@gmail.com>
2007-05-03 18:46:46 +0000

    Additional files that will be used for development.

---
+++ pack.pl
@@ -0,0 +1,39 @@
+
+
+open IN, "webforms2.js";
+$source = join "", <IN>;
+close IN;
+
+$source =~ s{\bwebforms2\.js\b}
+            {webforms2-p.js}g;
+#$source =~ s{(/\*\@cc_on \@\*/\n/\*\@if \(\@_win32\).+?/\*\@end \@\*/)}
+#            {MSIE_ONDOMCONTENTLOADED();}s;
+#(?=/\*\s+cssQuery)
+$source =~ s{(if\(!window\.RepetitionElement.+)} 
+            {jsPack($1) . "\n\n"}es;
+
+
+#$source =~ s{var RepetitionElement.+?(?=/\*\@cc_on \@\*/)}
+#            {SOURCE1}es;
+#$source =~ s{var RepetitionElement.+?(?=/\*\@cc_on \@\*/)}
+#            {SOURCE1}es;
+#$source =~ s{(?<=/\*\@cc_on \@\*/\n/\*\@if \(\@_win32\))(.+?)(?=/\*\@end \@\*/)}
+#            {SOURCE2}es;
+
+open OUT, ">webforms2-p.js";
+print OUT $source;
+close OUT;
+
+
+sub jsPack {
+	open TEMP, ">~topack.js";
+	print TEMP shift;
+	close TEMP;
+	
+	chdir('./packer.perl/');
+	my $packed = `perl jsPacker.pl -q -f -i ../~topack.js`; #-e62
+	chdir('..');
+	
+	unlink "~topack.js";
+	return $packed;
+}
---
+++ testsuite/~testcases/compile.pl
@@ -0,0 +1,134 @@
+
+
+use Win32::File qw/GetAttributes SetAttributes ARCHIVE NORMAL/;
+
+
+while(1){
+	@files = grep /^\d/, sort <*.html>;
+	$doupdate = 0;
+	foreach(@files){
+		GetAttributes($_, $attrib);
+		if($attrib & ARCHIVE){
+			$doupdate++;
+		}
+	}
+	
+	if(!$doupdate){
+		sleep 1;
+		next;
+	}
+	
+	
+	open S, "testcase.template.html";
+	$testcaseTemplate = join "", <S>;
+	close S;
+	$counter = 1;
+	
+	open S, "../../repetition-model.js";
+	$s = join '', <S>;
+	$s =~ /Version:\s+(.+?)\s+\((.+?)\)/;
+	$implementationVersion = $1;
+	$implementationDate = $2;
+	close S;
+
+	%testcases = ();
+
+	for($i = 0; $i < @files; $i++){
+		#next if not $files[$i] =~ /011/; #REMOVE
+		
+		print "$files[$i]\n";
+		$testcaseNumber = sprintf("%03d", $counter);
+		
+		$testcasePage = $testcaseTemplate;
+		
+		open FILE, $files[$i];
+		$_ = join '', <FILE>;
+		close FILE;
+		
+		m{<title.*?>(.+?)</title>}s;
+		$testcaseTitle = $1;
+		$testcasePage =~ s{__testcaseTitle__}{$testcaseTitle}g;
+		
+		
+		m{<div id="testcaseDesc">(.+?)</div>}s;
+		$testcaseDesc = $1;
+		$testcaseDesc =~ s{\s+}{ }g;
+		$testcasePage =~ s{__testcaseDesc__}{$testcaseDesc};
+		
+		#__testcaseShortDesc__
+		m{<meta name="description" content="(.*?)" />}s;
+		$testcaseShortDesc = $1;
+		$testcasePage =~ s{__testcaseShortDesc__}{$testcaseShortDesc}g;
+		
+		$testcases{$testcaseNumber} = {
+			title => $testcaseTitle,
+			shortDesc => $testcaseShortDesc
+		};
+		
+		m{<!-- BEGIN TEST CASE -->(.+?)<!-- END TEST CASE -->}s;
+		$testcaseCode = $1;
+		$testcasePage =~ s{__testcaseCode__}{$testcaseCode};
+		
+		m{<!-- BEGIN HEAD -->(.+?)<!-- END HEAD -->}s;
+		$testcaseHeadCode = $1;
+		$testcasePage =~ s{__testcaseHeadCode__}{$testcaseHeadCode};
+		
+		$testcasePage =~ s{__testcaseNumber__}{$counter}eg;
+		
+		$testcasePage =~ s{__implementationVersion__}{$implementationVersion}g;
+		$testcasePage =~ s{__implementationDate__}{$implementationDate}g;
+		
+		if($i == 0){
+			$testcasePage =~ s{<!-- prev -->(.+?)<!-- /prev -->}{}g;
+		}
+		$testcasePage =~ s{__previousNumber__}{sprintf("%03d", $counter-1)}ge;
+		$testcasePage =~ s{__previousTitle__}{getPageTitle(sprintf("%03d.html", $counter-1))}ge;
+		if($i == @files-1){
+			$testcasePage =~ s{<!-- next -->(.+?)<!-- /next -->}{}g;
+		}
+		$testcasePage =~ s{__nextNumber__}{sprintf("%03d", $counter+1)}ge;
+		$testcasePage =~ s{__nextTitle__}{getPageTitle(sprintf("%03d.html", $counter+1))}ge;
+		
+		open OUT, ">../$testcaseNumber.html";
+		print OUT $testcasePage;
+		close OUT;
+		SetAttributes($files[$i], NORMAL);
+		
+		$counter++;
+	}
+
+
+
+	#TEST SUITE INDEX
+
+	open S, "index.template.html";
+	$indexTemplate = join "", <S>;
+	close S;
+
+	$list = "";
+	foreach(sort keys %testcases){
+		$list .= "<li><a href='$_.html'>" . $testcases{$_}{title} . "</a>";
+		
+		$list .= ": " . $testcases{$_}{shortDesc} if $testcases{$_}{shortDesc};
+		
+		$list .= "</li>\n";
+	}
+
+	$indexTemplate =~ s{__testcaseList__}{$list};
+	$indexTemplate =~ s{__implementationVersion__}{$implementationVersion}g;
+	$indexTemplate =~ s{__implementationDate__}{$implementationDate}g;
+
+	open OUT, ">../index.html";
+	print OUT $indexTemplate;
+	close OUT;
+	
+	#exit; #REMOVE
+}
+
+
+sub getPageTitle {
+	open FILE, shift;
+	$_ = join('', <FILE>);
+	m{<title.*?>(.+?)</title>}s;
+	return $1;
+}
